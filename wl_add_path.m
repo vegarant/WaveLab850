808cd69384fb43fe244f0c813c234904de039b2e
Vegard Antun <vegarant@student.matnat.uio.no>
2018-07-19 11:52:59 +0100

    Changed the way WaveLab add paths

---
+++ wl_add_path.m
@@ -0,0 +1,57 @@
+
+
+wavelab_root = pwd();
+
+addpath(fullfile(wavelab_root, 'Papers'));
+addpath(fullfile(wavelab_root, 'Papers', 'Blocky')); 
+addpath(fullfile(wavelab_root, 'Papers', 'Correl')); 
+addpath(fullfile(wavelab_root, 'Papers', 'Tour')); 
+addpath(fullfile(wavelab_root, 'Papers', 'RiskAnalysis')); 
+addpath(fullfile(wavelab_root, 'Papers', 'SpinCycle')); 
+addpath(fullfile(wavelab_root, 'Papers', 'MinEntSeg')); 
+addpath(fullfile(wavelab_root, 'Papers', 'Adapt')); 
+addpath(fullfile(wavelab_root, 'Papers', 'MIPT')); 
+addpath(fullfile(wavelab_root, 'Papers', 'Asymp')); 
+addpath(fullfile(wavelab_root, 'Papers', 'VillardDeLans')); 
+addpath(fullfile(wavelab_root, 'Papers', 'ShortCourse')); 
+addpath(fullfile(wavelab_root, 'Papers', 'Ideal')); 
+addpath(fullfile(wavelab_root, 'TimeFrequency')); 
+addpath(fullfile(wavelab_root, 'MEXSource')); 
+addpath(fullfile(wavelab_root, 'Packets')); 
+addpath(fullfile(wavelab_root, 'Packets', 'One-D')); 
+addpath(fullfile(wavelab_root, 'Packets', 'Two-D')); 
+addpath(fullfile(wavelab_root, 'Invariant')); 
+addpath(fullfile(wavelab_root, 'Documentation')); 
+addpath(fullfile(wavelab_root, 'FastAlgorithms')); 
+addpath(fullfile(wavelab_root, 'Workouts')); 
+addpath(fullfile(wavelab_root, 'Workouts', 'MultiFractal')); 
+addpath(fullfile(wavelab_root, 'Workouts', 'MatchingPursuit')); 
+addpath(fullfile(wavelab_root, 'Workouts', 'BestOrthoBasis')); 
+addpath(fullfile(wavelab_root, 'Workouts', 'Toons')); 
+addpath(fullfile(wavelab_root, 'Biorthogonal')); 
+addpath(fullfile(wavelab_root, 'Datasets')); 
+addpath(fullfile(wavelab_root, 'Pursuit')); 
+addpath(fullfile(wavelab_root, 'Orthogonal')); 
+addpath(fullfile(wavelab_root, 'Browsers')); 
+addpath(fullfile(wavelab_root, 'Browsers', 'WaveTour')); 
+addpath(fullfile(wavelab_root, 'Browsers', 'One-D')); 
+addpath(fullfile(wavelab_root, 'Fractals')); 
+addpath(fullfile(wavelab_root, 'DeNoising')); 
+addpath(fullfile(wavelab_root, 'Continuous')); 
+addpath(fullfile(wavelab_root, 'Meyer')); 
+addpath(fullfile(wavelab_root, 'Interpolating')); 
+addpath(fullfile(wavelab_root, 'Books')); 
+addpath(fullfile(wavelab_root, 'Books', 'WaveTour')); 
+addpath(fullfile(wavelab_root, 'Books', 'WaveTour', 'WTCh02')); 
+addpath(fullfile(wavelab_root, 'Books', 'WaveTour', 'WTCh04')); 
+addpath(fullfile(wavelab_root, 'Books', 'WaveTour', 'WTCh05')); 
+addpath(fullfile(wavelab_root, 'Books', 'WaveTour', 'WTCh06')); 
+addpath(fullfile(wavelab_root, 'Books', 'WaveTour', 'WTCh07')); 
+addpath(fullfile(wavelab_root, 'Books', 'WaveTour', 'WTCh08')); 
+addpath(fullfile(wavelab_root, 'Books', 'WaveTour', 'WTCh09')); 
+addpath(fullfile(wavelab_root, 'Books', 'WaveTour', 'WTCh10')); 
+addpath(fullfile(wavelab_root, 'Median')); 
+addpath(fullfile(wavelab_root, 'Median', 'HigherDegree')); 
+addpath(fullfile(wavelab_root, 'Utilities')); 
+
+
